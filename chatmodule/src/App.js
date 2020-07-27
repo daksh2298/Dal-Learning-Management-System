d2ee59f238d2524b1f19bf95a2439bdf8071be40
Arthy <arthy98@gmail.com>
2020-07-27 00:39:28 -0300

    chat module

---
+++ chatmodule/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ChatBox from './chatBox';
+
+function App() {
+  return (
+    <div className="App">
+      <ChatBox/>
+    </div>
+  );
+}
+
+export default App;
